1e3fbc69d4c3da3affa506b00a81194eabf83864
user <user@WIN-3HCRIBMUHPD>
2023-10-14 00:12:12 +0300

    Добавьте файлы проекта.

---
+++ Интернет магазин Черешня/Информация о пользователях.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Интернет_магазин_Черешня
+{
+    /// <summary>
+    /// Логика взаимодействия для Информация_о_пользователях.xaml
+    /// </summary>
+    public partial class Информация_о_пользователях : Window
+    {
+        public Информация_о_пользователях()
+        {
+            InitializeComponent();
+        }
+    }
+}
